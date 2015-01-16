1155afa1e5586a103c736c46f3e9c6bf95bac870
otomoassassin <jakkrit.kamonrat@gmail.com>
2015-01-16 16:51:33 +0700

    week1

---
+++ otomoassassin.playground/section-1.swift
@@ -0,0 +1,61 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let number1=9;
+let number2=30;
+var sum=number1+number2;
+sum += 1;
+
+let massage1="If I Clash You"
+let massage2="Wiparat"
+
+println(massage1+" "+massage2);
+
+if (number1<10){
+    let number2=3;
+}else{
+    let number1=15;
+}
+
+var i=1
+while(i<=number1){
+     println(i)
+        i += 1
+    
+    
+}
+
+var ar1=["otomo","otome","Aiyak","jakkrit"];
+
+println("Hello \(ar1[0...3]) How are you?")
+
+var ar2=["kirimaru","yak"];
+ar2 += ["kirimaru"]
+
+ar2.count
+
+var array1 = ["abc","bcd","cdf"];
+
+for (var i=0; i < array1.count; i++ ){
+    println(array1[i])
+}
+var array2 = ["a","b","c"];
+var str1 = ""
+for str1 in array2{
+    println(str1)
+
+}
+
+var animal=["koob": 4,"mar": 4,"kon": 2,"mangmoom": 8];
+for(sad,kar) in animal{
+println("\(sad)s have \(kar)jo jo ")
+}
+
+
+
+
+
+
+
